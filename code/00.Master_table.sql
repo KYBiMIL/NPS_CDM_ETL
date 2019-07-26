4d3869b1657ba92be46988f0a0cba78e7ced7f56
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-07-26 15:18:09 +0900

    Update 00.Master_table.sql

--- code/00.Master_table.sql
+++ code/00.Master_table.sql
@@ -20,45 +20,29 @@
  1. 테이블 생성
     : 일련번호(PK), 소스 테이블, person_id, 30T, 40T, 60T, 검진 테이블의 Primary key들을 컬럼으로 하는 테이블 생성
 ***************************************/  
-
-select banner from v$version where rownum = 1;
-
-CREATE TABLE cohort_cdm.SEQ_MASTER (
-	master_seq  NUMBER	primary key,
-	source_table    CHAR(3)	NOT NULL, -- 30T, 40T, 60T는 130, 140, 160. 검진은 'GJT'
-	person_id   INTEGER	NOT NULL, -- 모두
-	key_seq NUMBER	NULL, -- 30T, 40T, 60T
-	seq_no  NUMERIC(4)	NULL, -- 30T, 40T, 60T
-	hchk_year   VARCHAR(4) NULL -- 검진	
-);
+CREATE TABLE @ResultDatabaseSchema.SEQ_MASTER (
+	master_seq		BIGINT	identity(1, 1) PRIMARY KEY,
+	source_table	CHAR(3)	NOT NULL, -- 30T, 40T, 60T는 130, 140, 160. 검진은 'GJT'
+	person_id		INT	NOT NULL, -- 모두
+	key_seq			BIGINT	NULL, -- 30T, 40T, 60T
+	seq_no			NUMERIC(4)	NULL, -- 30T, 40T, 60T
+	hchk_year		CHAR(4)	NULL, -- 검진	
+)
 -- 607738697
---identifiy for Oracle 11g
-CREATE SEQUENCE master_seq START WITH 1;
 
-CREATE OR REPLACE TRIGGER master_bir 
-BEFORE INSERT ON SEQ_MASTER 
-FOR EACH ROW
-
-BEGIN
-  SELECT master_seq.NEXTVAL
-  INTO   :new.master_seq
-  FROM   dual;
-END;
-/
---
 /**************************************
  2. 30T에 대한 데이터 입력
     : 일련번호는 3000000001, 30억대부터 시작
 ***************************************/
 -- 1) 일련번호 초기화
-DBCC CHECKIDENT('seq_master', RESEED, 3000000001);
+DBCC CHECKIDENT('seq_master', RESEED, 3000000001)
 
 -- 2) 데이터 입력
-INSERT INTO cohort_cdm.SEQ_MASTER
+INSERT INTO @ResultDatabaseSchema.SEQ_MASTER
 	(source_table, person_id, key_seq, seq_no)
 SELECT '130', b.person_id, a.key_seq, a.seq_no
-FROM cohort_cdm.NHID_30T a, cohort_cdm.NHID_20T b
-WHERE a.key_seq=b.key_seq;
+FROM @NHISDatabaseSchema.@NHIS_30T a, @NHISDatabaseSchema.@NHIS_20T b
+WHERE a.key_seq=b.key_seq
 
 
 /**************************************
@@ -69,10 +53,10 @@ WHERE a.key_seq=b.key_seq;
 DBCC CHECKIDENT('seq_master', RESEED, 4000000000)
 
 -- 2) 데이터 입력
-INSERT INTO cohort_cdm.SEQ_MASTER
+INSERT INTO @ResultDatabaseSchema.SEQ_MASTER
 	(source_table, person_id, key_seq, seq_no)
 SELECT '140', b.person_id, a.key_seq, a.seq_no
-FROM cohort_cdm.NHID_40T a, cohort_cdm.NHID_20T b
+FROM @NHISDatabaseSchema.@NHIS_40T a, @NHISDatabaseSchema.@NHIS_20T b
 WHERE a.key_seq=b.key_seq
 
 
@@ -84,10 +68,10 @@ WHERE a.key_seq=b.key_seq
 DBCC CHECKIDENT('seq_master', RESEED, 6000000000)
 
 -- 2) 데이터 입력
-INSERT INTO cohort_cdm.SEQ_MASTER
+INSERT INTO @ResultDatabaseSchema.SEQ_MASTER
 	(source_table, person_id, key_seq, seq_no)
 SELECT '160', b.person_id, a.key_seq, a.seq_no
-FROM cohort_cdm.NHID_60T a, cohort_cdm.NHID_20T b
+FROM @NHISDatabaseSchema.@NHIS_60T a, @NHISDatabaseSchema.@NHIS_20T b
 WHERE a.key_seq=b.key_seq
 
 
@@ -100,14 +84,15 @@ WHERE a.key_seq=b.key_seq
 DBCC CHECKIDENT('seq_master', RESEED, 800000000000)
 
 -- 2) 데이터 입력
-INSERT INTO cohort_cdm.SEQ_MASTER
+INSERT INTO @ResultDatabaseSchema.SEQ_MASTER
 	(source_table, person_id, hchk_year)
 SELECT 'GJT', person_id, hchk_year
-FROM cohort_cdm.NHID_GJ
+FROM @NHISDatabaseSchema.@NHIS_GJ
 GROUP BY hchk_year, person_id
 
 
 /**************************************
  6. 일련번호 자동증가 비활성화시킴
 ***************************************/
-DBCC CHECKIDENT('seq_master', NORESEED);
+DBCC CHECKIDENT('seq_master', NORESEED)
+
