193320b7c8adb1f2ee25141d49d5add414c4e15c
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-07-29 14:31:05 +0900

    Update 06.Visit_occurrence.sql

--- code/06.Visit_occurrence.sql
+++ code/06.Visit_occurrence.sql
@@ -4,12 +4,12 @@
  --Date: 2017.01.26
  
  @NHISDatabaseSchema : DB containing NHIS National Sample cohort DB
- @NHIS_JK: JK table in NHIS NSC
- @NHIS_20T: 20 table in NHIS NSC
- @NHIS_30T: 30 table in NHIS NSC
- @NHIS_40T: 40 table in NHIS NSC
- @NHIS_60T: 60 table in NHIS NSC
- @NHIS_GJ: GJ table in NHIS NSC
+ @NHID_JK: JK table in NHIS NSC
+ @NHID_20T: 20 table in NHIS NSC
+ @NHID_30T: 30 table in NHIS NSC
+ @NHID_40T: 40 table in NHIS NSC
+ @NHID_60T: 60 table in NHIS NSC
+ @NHID_GJ: GJ table in NHIS NSC
  --Description: Visit_occurrence 테이블 생성
  --Generating Table: VISIT_OCCURRENCE
 ***************************************/
@@ -17,7 +17,7 @@
 /**************************************
  1. 테이블 생성
 ***************************************/ 
-CREATE TABLE @ResultDatabaseSchema.VISIT_OCCURRENCE (
+CREATE TABLE cohort_cdm.VISIT_OCCURRENCE (                 --데이터 유형이 부적합
 	visit_occurrence_id	bigint	primary key,
 	person_id			integer	not null,
 	visit_concept_id	integer	not null,
@@ -32,10 +32,12 @@ CREATE TABLE @ResultDatabaseSchema.VISIT_OCCURRENCE (
 	visit_source_concept_id	integer
 );
 
+
+
 /**************************************
  2. 데이터 입력
 ***************************************/ 
-insert into @ResultDatabaseSchema.VISIT_OCCURRENCE (
+insert into cohort_cdm.VISIT_OCCURRENCE (
 	visit_occurrence_id, person_id, visit_concept_id, visit_start_date, visit_start_time,
 	visit_end_date, visit_end_time, visit_type_concept_id, provider_id, care_site_id,
 	visit_source_value, visit_source_concept_id
@@ -61,12 +63,11 @@ select
 	ykiho_id as care_site_id,
 	key_seq as visit_source_value,
 	null as visit_source_concept_id
-from @NHISDatabaseSchema.@NHIS_20T
-;
+from cohort_cdm.NHID_20T;
 
 
 --건강검진 INSERT
-insert into @ResultDatabaseSchema.VISIT_OCCURRENCE (
+insert into cohort_cdm.VISIT_OCCURRENCE (
 	visit_occurrence_id, person_id, visit_concept_id, visit_start_date, visit_start_time,
 	visit_end_date, visit_end_time, visit_type_concept_id, provider_id, care_site_id,
 	visit_source_value, visit_source_concept_id
@@ -84,5 +85,4 @@ select
 	null as care_site_id,
 	b.master_seq as visit_source_value,
 	null as visit_source_concept_id
-from @NHISDatabaseSchema.@NHIS_GJ a JOIN @ResultDatabaseSchema.seq_master b on a.person_id=b.person_id and a.hchk_year=b.hchk_year
-;
+from cohort_cdm.NHID_GJ a JOIN cohort_cdm.seq_master b on a.person_id=b.person_id and a.hchk_year=b.hchk_year;
