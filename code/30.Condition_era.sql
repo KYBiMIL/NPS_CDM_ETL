a2300a890189dc0314c2465ee344cdaf1c347f3b
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-08-05 09:44:01 +0900

    Update 30.Condition_era.sql

--- code/30.Condition_era.sql
+++ code/30.Condition_era.sql
@@ -2,14 +2,14 @@
  --encoding : UTF-8
  --Author: OHDSI
   
-@NHISDatabaseSchema : DB containing NHIS National Sample cohort DB
-@ResultDatabaseSchema : DB for NHIS-NSC in CDM format
+cohort_cdm : DB containing NHIS National Sample cohort DB
+cohort_cdm : DB for NHIS-NSC in CDM format
  
  --Description: OHDSI에서 생성한 condition_era 생성 쿼리
                총 3개의 temp table이 만들어짐
-			   1) #cteConditionTarget
-			   2) #cteEndDates
-			   3) #cteConditionEnds
+			   1) cteConditionTarget
+			   2) cteEndDates
+			   3) cteConditionEnds
  --Generating Table: CONDITION_ERA
 ***************************************/
 
@@ -28,7 +28,7 @@
 /**************************************
  1. condition_era 테이블 생성
 ***************************************/ 
-CREATE TABLE @NHISDatabaseSchema.CONDITION_ERA  (
+CREATE TABLE cohort_cdm.CONDITION_ERA  (
      condition_era_id					INTEGER	 identity(1,1)    NOT NULL , 
      person_id							INTEGER     NOT NULL ,
      condition_concept_id				INTEGER   NOT NULL ,
@@ -48,7 +48,7 @@ SELECT
 	condition_concept_id, 
 	condition_start_date, 
 	COALESCE(NULLIF(condition_end_date,NULL), dateadd (day, 31, condition_start_date)) AS condition_end_date
-into #cteConditionTarget 
+into cteConditionTarget 
 FROM condition_occurrence;
 	
 	
@@ -57,7 +57,7 @@ SELECT
 	person_id
 	, condition_concept_id
 	, dateadd(day, -30, event_date) AS end_date 
-into #cteEndDates FROM
+into cteEndDates FROM
 (
 	SELECT
 		person_id
@@ -75,7 +75,7 @@ into #cteEndDates FROM
 			, condition_start_date AS event_date
 			, -1 AS event_type
 			, ROW_NUMBER() OVER (PARTITION BY person_id, condition_concept_id ORDER BY condition_start_date) AS start_ordinal
-		FROM #cteConditionTarget
+		FROM cteConditionTarget
 	
 		UNION ALL
 	
@@ -86,7 +86,7 @@ into #cteEndDates FROM
 			, dateadd( day,30,condition_end_date) 
 			, 1 AS event_type
 			, NULL
-		FROM #cteConditionTarget
+		FROM cteConditionTarget
 	) RAWDATA
 ) e
 WHERE (2 * e.start_ordinal) - e.overall_ord = 0;
@@ -98,8 +98,8 @@ SELECT
 	, c.condition_concept_id
 	, c.condition_start_date
 	, MIN(e.end_date) AS era_end_date
-into #cteConditionEnds FROM #cteConditionTarget c
-JOIN #cteEndDates e ON c.person_id = e.person_id AND c.condition_concept_id = e.condition_concept_id AND e.end_date >= c.condition_start_date
+into cteConditionEnds FROM cteConditionTarget c
+JOIN cteEndDates e ON c.person_id = e.person_id AND c.condition_concept_id = e.condition_concept_id AND e.end_date >= c.condition_start_date
 GROUP BY
         c.condition_occurrence_id
 	, c.person_id
@@ -111,13 +111,13 @@ GROUP BY
  3. 2단계: condition_era에 데이터 입력
 ***************************************/ 
 
-INSERT INTO @NHISDatabaseSchema.condition_era (person_id, condition_concept_id, condition_era_start_date, condition_era_end_date, condition_occurrence_count)
+INSERT INTO cohort_cdm.condition_era (person_id, condition_concept_id, condition_era_start_date, condition_era_end_date, condition_occurrence_count)
 SELECT
 	person_id
 	, condition_concept_id
 	, MIN(condition_start_date) AS condition_era_start_date
 	, era_end_date AS condition_era_end_date
 	, COUNT(*) AS condition_occurrence_count
-FROM #cteConditionEnds
+FROM cteConditionEnds
 GROUP BY person_id, condition_concept_id, era_end_date
 ORDER BY person_id, condition_concept_id
