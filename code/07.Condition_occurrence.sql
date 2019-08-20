3511fa513150dc70c6782dae8a7c148a8dcbc42b
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-08-20 13:40:00 +0900

    Update 07.Condition_occurrence.sql

--- code/07.Condition_occurrence.sql
+++ code/07.Condition_occurrence.sql
@@ -1,16 +1,16 @@
-/**************************************
+=/**************************************
  --encoding : UTF-8
  --Author: 이성원
  --Date: 2017.01.31
  
-cohort_cdm : DB containing NHIS National Sample cohort DB
-NHID_JK: JK table in NHIS NSC
-NHID_20T: 20 table in NHIS NSC
-NHID_30T: 30 table in NHIS NSC
-NHID_40T: 40 table in NHIS NSC
-NHID_60T: 60 table in NHIS NSC
-NHID_GJ: GJ table in NHIS NSC
-CONDITION_MAPPINGTABLE : mapping table between KCD and SNOMED-CT
+@NHISDatabaseSchema : DB containing NHIS National Sample cohort DB
+@NHIS_JK: JK table in NHIS NSC
+@NHIS_20T: 20 table in NHIS NSC
+@NHIS_30T: 30 table in NHIS NSC
+@NHIS_40T: 40 table in NHIS NSC
+@NHIS_60T: 60 table in NHIS NSC
+@NHIS_GJ: GJ table in NHIS NSC
+@CONDITION_MAPPINGTABLE : mapping table between KCD and SNOMED-CT
  --Description: Condition_occurrence 테이블 생성
  --Generating Table: CONDITION_OCCURRENCE
 ***************************************/
@@ -19,7 +19,7 @@ CONDITION_MAPPINGTABLE : mapping table between KCD and SNOMED-CT
  1. 테이블 생성
 ***************************************/ 
 CREATE TABLE cohort_cdm.CONDITION_OCCURRENCE ( 
-     condition_occurrence_id		number			PRIMARY KEY, 
+     condition_occurrence_id		NUMBER			PRIMARY KEY, 
      person_id						INTEGER			NOT NULL , 
      condition_concept_id			INTEGER			NOT NULL , 
      condition_start_date			DATE			NOT NULL , 
@@ -27,26 +27,10 @@ CREATE TABLE cohort_cdm.CONDITION_OCCURRENCE (
      condition_type_concept_id		INTEGER			NOT NULL , 
      stop_reason					VARCHAR(20), 
      provider_id					INTEGER, 
-     visit_occurrence_id			number, 
+     visit_occurrence_id			NUMBER, 
      condition_source_value			VARCHAR(50),
 	 condition_source_concept_id	VARCHAR(50)
 );
-
-create global temporary table cohort_cdm.CONDITTION_OCCURRENCE (
-    condition_occurrence_id		    number			PRIMARY KEY, 
-     person_id						INTEGER			NOT NULL , 
-     condition_concept_id			INTEGER			NOT NULL , 
-     condition_start_date			DATE			NOT NULL , 
-     condition_end_date				DATE, 
-     condition_type_concept_id		INTEGER			NOT NULL , 
-     stop_reason					VARCHAR(20), 
-     provider_id					INTEGER, 
-     visit_occurrence_id			number, 
-     condition_source_value			VARCHAR(50),
-	 condition_source_concept_id	VARCHAR(50)
-)
-on commit preserve rows;
-
 
 
 /**************************************
@@ -67,7 +51,7 @@ INSERT INTO cohort_cdm.CONDITION_OCCURRENCE
 	condition_type_concept_id, stop_reason, provider_id, visit_occurrence_id, condition_source_value, 
 	condition_source_concept_id)
 select 
-	to_number( to_char(m.master_seq) || to_char(ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc))) as condition_occurrence_id,
+	to_number(to_char(m.master_seq) || to_char(ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc))) as condition_occurrence_id,
 	--ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc) AS rank, m.seq_no,
 	m.person_id as person_id,
 	n.concept_id as condition_concept_id,
@@ -82,25 +66,6 @@ select
 from (
 	select
 		a.master_seq, a.person_id, a.key_seq, a.seq_no, b.recu_fr_dt,
-		case when b.form_cd in ('02', '04', '06', '07', '10', '12') then last_day('yyyymmdd', to_date(b.recu_fr_dt, 112)) 
-			when b.form_cd in ('03', '05', '08', '09', '11', '13', '20', '21', 'ZZ') and b.in_pat_cors_type in ('11', '21', '31') then last_day('yyyymmdd', to_date(b.recu_fr_dt, 112))
-			else to_date(b.recu_fr_dt, 112)
-		end as visit_end_date,
-		c.sick_sym,
-		case when c.sick_sym=b.main_sick then '44786627' --primary condition
-			when c.sick_sym=b.sub_sick then '44786629' --secondary condition
-			else '45756845' --third condition
-		end as sick_order,
-		case when b.sub_sick=c.sick_sym then 'Y' else 'N' end as sub_sick_yn
-	from (select master_seq, person_id, key_seq, seq_no from seq_master where source_table='140') a, 
-		cohort_cdm.NHID_20T b,
-		cohort_cdm.NHID_40T  c,
-		observation_period d --추가
-	where a.person_id=b.person_id
-	and a.key_seq=b.key_seq
-	and a.key_seq=c.key_seq
-	and a.seq_no=c.seq_no
-	and b.person_id=d.person_id --추가
-	and to_date(c.recu_fr_dt, 112) between d.observation_period_start_date and d.observation_period_end_date) as m, --추가
-	cohort_cdm.CONDITION_MAPPINGTABLE as n
-where m.sick_sym=n.kcdcode;
+		case when b.form_cd in ('02', '04', '06', '07', '10', '12') then to_date(b.recu_fr_dt, 112) + b.vscn - 1 
+			when b.form_cd in ('03', '05', '08', '09', '11', '13', '20', '21', 'ZZ') and b.in_pat_cors_type in ('11', '21', '31') then to_date(b.recu_fr_dt, 112) + b.vscn - 1
+	);
