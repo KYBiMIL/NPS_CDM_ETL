86ce6ff67a533d0d7dc1d8d1457593323e50b1c8
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-08-20 16:59:46 +0900

    Update 09.Procedure_occurrence.sql

--- code/09.Procedure_occurrence.sql
+++ code/09.Procedure_occurrence.sql
@@ -3,17 +3,17 @@
  --Author: 이성원
  --Date: 2017.02.13
  
-cohort_cdm : DB containing NHIS National Sample cohort DB
-cohort_cdm : DB for NHIS-NSC in CDM format
-NHID_JK: JK table in NHIS NSC
-NHID_20T: 20 table in NHIS NSC
-NHID_30T: 30 table in NHIS NSC
-NHID_40T: 40 table in NHIS NSC
-NHID_60T: 60 table in NHIS NSC
-NHID_GJ: GJ table in NHIS NSC
-CONDITION_MAPPINGTABLE : mapping table between KCD and OMOP vocabulary
-DRUG_MAPPINGTABLE : mapping table between EDI and OMOP vocabulary
-PROCEDURE_MAPPINGTABLE : mapping table between Korean procedure and OMOP vocabulary
+@NHISDatabaseSchema : DB containing NHIS National Sample cohort DB
+@ResultDatabaseSchema : DB for NHIS-NSC in CDM format
+@NHIS_JK: JK table in NHIS NSC
+@NHIS_20T: 20 table in NHIS NSC
+@NHIS_30T: 30 table in NHIS NSC
+@NHIS_40T: 40 table in NHIS NSC
+@NHIS_60T: 60 table in NHIS NSC
+@NHIS_GJ: GJ table in NHIS NSC
+@CONDITION_MAPPINGTABLE : mapping table between KCD and OMOP vocabulary
+@DRUG_MAPPINGTABLE : mapping table between EDI and OMOP vocabulary
+@PROCEDURE_MAPPINGTABLE : mapping table between Korean procedure and OMOP vocabulary
  
  --Description: Procedure_occurrence 테이블 생성
 			   * 30T(진료), 60T(처방전) 테이블에서 각각 ETL을 수행해야 함
@@ -77,7 +77,7 @@ and a.key_seq=c.key_seq
  2. 테이블 생성
 ***************************************/ 
 CREATE TABLE cohort_cdm.PROCEDURE_OCCURRENCE ( 
-     procedure_occurrence_id	NUMBER			PRIMARY KEY, 
+     procedure_occurrence_id		NUMBER			PRIMARY KEY, 
      person_id						INTEGER			NOT NULL, 
      procedure_concept_id			INTEGER			NOT NULL, 
      procedure_date					DATE			NOT NULL, 
@@ -91,24 +91,6 @@ CREATE TABLE cohort_cdm.PROCEDURE_OCCURRENCE (
 	 qualifier_source_value			VARCHAR(50)		NULL
     );
 
-
-create global temporary table cohort_cdm.PROCEDURE_OCCURRENCE
-(
-	procedure_occurrence_id		NUMBER			PRIMARY KEY, 
-     person_id						INTEGER			NOT NULL, 
-     procedure_concept_id			INTEGER			NOT NULL, 
-     procedure_date					DATE			NOT NULL, 
-     procedure_type_concept_id		INTEGER			NOT NULL,
-	 modifier_concept_id			INTEGER			NULL,
-	 quantity						INTEGER			NULL, 
-     provider_id					INTEGER			NULL, 
-     visit_occurrence_id			NUMBER			NULL, 
-     procedure_source_value			VARCHAR(50)		NULL,
-	 procedure_source_concept_id	INTEGER			NULL,
-	 qualifier_source_value			VARCHAR(50)		NULL
-	
-)
-on commit preserve rows;
 
 /**************************************
  3. 30T를 이용하여 데이터 입력
@@ -131,9 +113,9 @@ SELECT
 	null as procedure_source_concept_id,
 	null as qualifier_source_value
 FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd, 
-			case when x.mdcn_exec_freq is not null and isnumeric(x.mdcn_exec_freq)=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
-			case when x.dd_mqty_exec_freq is not null and isnumeric(x.dd_mqty_exec_freq)=1 and cast(x.dd_mqty_exec_freq as float) > '0' then cast(x.dd_mqty_exec_freq as float) else 1 end as dd_mqty_exec_freq,
-			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
+			case when x.mdcn_exec_freq is not null and REGEXP_INSTR(x.mdcn_exec_freq,'^[+-]?\d*(\.?\d*)$')=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
+			case when x.dd_mqty_exec_freq is not null and REGEXP_INSTR(x.dd_mqty_exec_freq,'^[+-]?\d*(\.?\d*)$')=1 and cast(x.dd_mqty_exec_freq as float) > '0' then cast(x.dd_mqty_exec_freq as float) else 1 end as dd_mqty_exec_freq,
+			case when x.dd_mqty_freq is not null and REGEXP_INSTR(x.dd_mqty_freq,'^[+-]?\d*(\.?\d*)$')=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
 			y.master_seq, y.person_id
 	FROM cohort_cdm.NHID_30T x, 
 		 (select master_seq, key_seq, seq_no, person_id from seq_master where source_table='130') y
@@ -150,7 +132,7 @@ INSERT INTO cohort_cdm.PROCEDURE_OCCURRENCE
 	modifier_concept_id, quantity, provider_id, visit_occurrence_id, procedure_source_value, 
 	procedure_source_concept_id, qualifier_source_value)
 SELECT 
-	to_number( to_char(a.master_seq) || to_char(row_number() over (partition by a.key_seq, a.seq_no order by b.concept_id))) as procedure_occurrence_id,
+	to_number(to_char(a.master_seq) || to_char(row_number() over (partition by a.key_seq, a.seq_no order by b.concept_id))) as procedure_occurrence_id,
 	a.person_id as person_id,
 	CASE WHEN b.concept_id IS NOT NULL THEN b.concept_id ELSE 0 END as procedure_concept_id,
 	to_char(a.recu_fr_dt, 112) as procedure_date,
@@ -163,9 +145,9 @@ SELECT
 	null as procedure_source_concept_id,
 	null as qualifier_source_value
 FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd, 
-			case when x.mdcn_exec_freq is not null and isnumeric(x.mdcn_exec_freq)=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
-			case when x.dd_exec_freq is not null and isnumeric(x.dd_exec_freq)=1 and cast(x.dd_exec_freq as float) > '0' then cast(x.dd_exec_freq as float) else 1 end as dd_exec_freq,
-			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
+			case when x.mdcn_exec_freq is not null and REGEXP_INSTR(x.mdcn_exec_freq,'^[+-]?\d*(\.?\d*)$')=1 and cast(x.mdcn_exec_freq as float) > '0' then cast(x.mdcn_exec_freq as float) else 1 end as mdcn_exec_freq,
+			case when x.dd_exec_freq is not null and REGEXP_INSTR(x.dd_exec_freq,'^[+-]?\d*(\.?\d*)$')=1 and cast(x.dd_exec_freq as float) > '0' then cast(x.dd_exec_freq as float) else 1 end as dd_exec_freq,
+			case when x.dd_mqty_freq is not null and REGEXP_INSTR(x.dd_mqty_freq,'^[+-]?\d*(\.?\d*)$')=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
 			y.master_seq, y.person_id
 	FROM cohort_cdm.NHID_60T x, 
 		 (select master_seq, key_seq, seq_no, person_id from seq_master where source_table='160') y
