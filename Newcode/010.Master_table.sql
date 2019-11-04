350c408699524845a55c23c59cc8581a374b6326
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-11-04 15:36:59 +0900

    Update 010.Master_table.sql

--- Newcode/010.Master_table.sql
+++ Newcode/010.Master_table.sql
@@ -22,29 +22,36 @@
  1. Create table
     : serial number(PK), source table, person_id, primary keys of 30T, 40T, 60T, GJ, JK tables
 ***************************************/  
-CREATE TABLE @NHISNSC_database.SEQ_MASTER (
-	master_seq		BIGINT	identity(1, 1) PRIMARY KEY,
-	source_table	CHAR(3)	NOT NULL, -- 30T = 130, 40T = 140, 60T = 160 GJ ='GJT', JK = 'JKT'
-	person_id		INT	NOT NULL, 
-	key_seq			BIGINT	NULL, -- 30T, 40T, 60T
+CREATE TABLE SEQ_MASTER (
+	master_seq		NUMBER PRIMARY KEY,
+	source_table	CHAR(3) NULL, -- 30T = 130, 40T = 140, 60T = 160 GJ ='GJT', JK = 'JKT'
+	person_id		INTEGER	NULL, 
+	key_seq			NUMBER	NULL, -- 30T, 40T, 60T
 	seq_no			NUMERIC(4)	NULL, -- 30T, 40T, 60T
 	hchk_year		CHAR(4)	NULL, -- GJ
-	stnd_y			CHAR(4) NULL, -- JK 
-)
-
+	stnd_y			CHAR(4) NULL -- JK 
+);
 
 /**************************************
  2. Insert data of 30T
     : serial number is starting from 3000000001
 ***************************************/
 -- 1) Reset the serial number
-DBCC CHECKIDENT('@NHISNSC_database.seq_master', RESEED, 3000000000);
+create SEQUENCE master_seq START WITH 3000000001 increment by 1;
 
+CREATE OR REPLACE TRIGGER master_bir 
+BEFORE INSERT ON SEQ_MASTER 
+FOR EACH ROW
+BEGIN
+  SELECT master_seq.NEXTVAL
+  INTO   :new.master_seq
+  FROM   dual;
+END;
 -- 2) Insert data
-INSERT INTO @NHISNSC_database.SEQ_MASTER
+INSERT INTO cohort_cdm.SEQ_MASTER
 	(source_table, person_id, key_seq, seq_no)
 SELECT '130', b.person_id, a.key_seq, a.seq_no
-FROM @NHISNSC_rawdata.@NHIS_30T a, @NHISNSC_rawdata.@NHIS_20T b
+FROM cohort_cdm.NHID_30T a, cohort_cdm.NHID_20T b
 WHERE a.key_seq=b.key_seq
 ;
 
@@ -53,13 +60,13 @@ WHERE a.key_seq=b.key_seq
     : serial number is starting from 4000000001
 ***************************************/
 -- 1) Reset the serial number
-DBCC CHECKIDENT('@NHISNSC_database.seq_master', RESEED, 4000000000);
+CREATE SEQUENCE master_seq START WITH 4000000001 increment by 1;
 
 -- 2) Insert data
-INSERT INTO @NHISNSC_database.SEQ_MASTER
+INSERT INTO cohort_cdm.SEQ_MASTER
 	(source_table, person_id, key_seq, seq_no)
 SELECT '140', b.person_id, a.key_seq, a.seq_no
-FROM @NHISNSC_rawdata.@NHIS_40T a, @NHISNSC_rawdata.@NHIS_20T b
+FROM cohort_cdm.NHID_40T a, cohort_cdm.NHID_20T b
 WHERE a.key_seq=b.key_seq
 ;
 
@@ -68,13 +75,13 @@ WHERE a.key_seq=b.key_seq
     : serial number is starting from 6000000001
 ***************************************/
 -- 1) Reset the serial number
-DBCC CHECKIDENT('@NHISNSC_database.seq_master', RESEED, 6000000000);
+CREATE SEQUENCE master_seq START WITH 6000000001 increment by 1;
 
 -- 2) Insert data
-INSERT INTO @NHISNSC_database.SEQ_MASTER
+INSERT INTO cohort_cdm.SEQ_MASTER
 	(source_table, person_id, key_seq, seq_no)
 SELECT '160', b.person_id, a.key_seq, a.seq_no
-FROM @NHISNSC_rawdata.@NHIS_60T a, @NHISNSC_rawdata.@NHIS_20T b
+FROM cohort_cdm.NHID_60T a, cohort_cdm.NHID_20T b
 WHERE a.key_seq=b.key_seq
 ;
 
@@ -84,13 +91,13 @@ WHERE a.key_seq=b.key_seq
 	: visit_occurrence_id is consisted with 12 numbers, match the numbers
 ***************************************/
 -- 1) Reset the serial number
-DBCC CHECKIDENT('@NHISNSC_database.seq_master', RESEED, 800000000000);
+CREATE SEQUENCE master_seq START WITH 800000000001 increment by 1;
 
 -- 2) Insert data
-INSERT INTO @NHISNSC_database.SEQ_MASTER
+INSERT INTO cohort_cdm.SEQ_MASTER
 	(source_table, person_id, hchk_year)
 SELECT 'GJT', person_id, hchk_year
-FROM @NHISNSC_rawdata.@NHIS_GJ
+FROM cohort_cdm.NHID_GJ
 GROUP BY hchk_year, person_id
 ;
 /**************************************
@@ -98,17 +105,17 @@ GROUP BY hchk_year, person_id
 	: serial number is starting from 900000000001
 **************************************/
 -- 1) Reset the serial number
-DBCC CHECKIDENT('@NHISNSC_database.seq_master', RESEED, 900000000000);
+CREATE SEQUENCE master_seq START WITH 900000000001 increment by 1;
 
 -- 2) Insert data
-INSERT INTO @NHISNSC_database.SEQ_MASTER
+INSERT INTO cohort_cdm.SEQ_MASTER
 	(source_table, person_id, stnd_y)
 SELECT 'JKT', person_id, STND_Y
-FROM @NHISNSC_rawdata.@NHIS_JK
+FROM cohort_cdm.NHID_JK
 GROUP BY STND_Y, person_id;
 ;
 
 /**************************************
  7. Deactivate the auto increment serial number
 ***************************************/
-DBCC CHECKIDENT('@NHISNSC_database.seq_master', NORESEED);
+drop sequence master_seq;
