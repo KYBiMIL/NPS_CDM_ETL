c707d4841083ce87c4fe81809794b1d7135a293a
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-11-06 08:45:21 +0900

    Update 150.Cost.sql

--- Newcode/150.Cost.sql
+++ Newcode/150.Cost.sql
@@ -56,7 +56,7 @@ CREATE TABLE @NHISNSC_database.COST (
 IF OBJECT_ID('tempdb..#mapping_table', 'U') IS NOT NULL
 	DROP TABLE #mapping_table;
 select a.source_code, a.target_concept_id, a.domain_id, REPLACE(a.invalid_reason, '', NULL) as invalid_reason 
-into #mapping_table
+into mapping_table
 from @Mapping_database.source_to_concept_map a join @Mapping_database.CONCEPT b on a.target_concept_id=b.concept_id
 where a.invalid_reason is null and b.invalid_reason is null;
 
@@ -72,7 +72,7 @@ where a.invalid_reason is null and b.invalid_reason is null;
 -- 1) Visit
 ---------------------------------------------------
 
-INSERT INTO @NHISNSC_database.COST
+INSERT INTO cohort_cdm.COST
 	(cost_id, cost_event_id, cost_domain_id, cost_type_concept_id, currency_concept_id,
 	total_charge, total_cost, total_paid, paid_by_payer, paid_by_patient,
 	paid_patient_copay, paid_patient_coinsurance, paid_patient_deductible, paid_by_primary, paid_ingredient_cost,
@@ -95,23 +95,22 @@ SELECT
 	null as paid_by_primary,
 	null as paid_ingredient_cost,
 	null as paid_dispensing_fee,
-	convert(bigint, convert(varchar, a.person_id) + left(convert(varchar, visit_start_date, 112), 4)) as payer_plan_period_id,
+	to_char(a.person_id) + left(convert(varchar, visit_start_date, 'yyyymmdd'), 4)) as payer_plan_period_id,
 	null as amount_allowed,
 	null as revenue_code_concept_id,
 	null as drg_concept_id,
 	null as revenue_code_source_value,
 	b.dmd_drg_no as drg_source_value
-from @NHISNSC_database.VISIT_OCCURRENCE a, @NHISNSC_rawdata.@NHIS_20T b
+from cohort_cdm.VISIT_OCCURRENCE a, cohort_cdm.NHID_20T b
 where a.visit_occurrence_id=b.key_seq
 and a.person_id=b.person_id;
-
 
 ---------------------------------------------------
 -- 2) Drug
 ---------------------------------------------------
 -- Check the duplicated IDs from Drug and Device tables
 /*
-select * from #mapping_table
+select * from mapping_table
 where source_code in (
 					select drug_source_value from @NHISNSC_database.DRUG_EXPOSURE a, @NHISNSC_database.DEVICE_EXPOSURE b
 					where a.drug_exposure_id=b.device_exposure_id and a.person_id=b.person_id
@@ -124,21 +123,21 @@ order by source_code
 delete from @NHISNSC_database.DRUG_EXPOSURE
 where drug_source_value in (select source_code from #mapping_table
 							where domain_id='drug' and source_code in (
-												select drug_source_value from @NHISNSC_database.DRUG_EXPOSURE a, @NHISNSC_database.DEVICE_EXPOSURE b
+												select drug_source_value from cohort_cdm.DRUG_EXPOSURE a, @NHISNSC_database.DEVICE_EXPOSURE b
 												where a.drug_exposure_id=b.device_exposure_id 
 													and a.person_id=b.person_id 
 											)
 								)
 -- Delete duplicated ID keys from temp mapping table
 delete from #mapping_table where domain_id='drug' and source_code in (
-												select drug_source_value from @NHISNSC_database.DRUG_EXPOSURE a, @NHISNSC_database.DEVICE_EXPOSURE b
+												select drug_source_value from cohort_cdm.DRUG_EXPOSURE a, @NHISNSC_database.DEVICE_EXPOSURE b
 												where a.drug_exposure_id=b.device_exposure_id 
 													and a.person_id=b.person_id )
 */
 
 --Insert data
 -- Source data: 30T
-INSERT INTO @NHISNSC_database.COST
+INSERT INTO cohort_cdm.COST
 	(cost_id, cost_event_id, cost_domain_id, cost_type_concept_id, currency_concept_id,
 	total_charge, total_cost, total_paid, paid_by_payer, paid_by_patient,
 	paid_patient_copay, paid_patient_coinsurance, paid_patient_deductible, paid_by_primary, paid_ingredient_cost,
@@ -171,7 +170,7 @@ from (select person_id, drug_exposure_id, drug_exposure_start_date
 	from @NHISNSC_database.DRUG_EXPOSURE
 	where drug_type_concept_id=38000180) a, 
 	(select m.master_seq, m.key_seq, m.seq_no, m.person_id, n.amt
-	from @NHISNSC_database.SEQ_MASTER m, @NHISNSC_rawdata.@NHIS_30T n
+	from cohort_cdm.SEQ_MASTER m, cohort_cdm.NHID_30T n
 	where m.source_table='130'
 		and m.key_seq=n.key_seq
 		and m.seq_no=n.seq_no) b
@@ -180,7 +179,7 @@ and a.person_id=b.person_id;
 
 
 -- Sourec data: 60T
-INSERT INTO @NHISNSC_database.COST
+INSERT INTO cohort_cdm.COST
 	(cost_id, cost_event_id, cost_domain_id, cost_type_concept_id, currency_concept_id,
 	total_charge, total_cost, total_paid, paid_by_payer, paid_by_patient,
 	paid_patient_copay, paid_patient_coinsurance, paid_patient_deductible, paid_by_primary, paid_ingredient_cost,
@@ -210,11 +209,11 @@ SELECT
 	null as revenue_code_source_value,
 	null as drg_source_value
 from (select person_id, drug_exposure_id, drug_exposure_start_date
-	from @NHISNSC_database.DRUG_EXPOSURE
+	from cohort_cdm.DRUG_EXPOSURE
 	where drug_type_concept_id=38000177) a, 
 	(select m.master_seq, m.key_seq, m.seq_no, m.person_id, n.amt
-	from (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='160') m, 
-	@NHISNSC_rawdata.@NHIS_60T n
+	from (select master_seq, key_seq, seq_no, person_id from cohort_cdm.SEQ_MASTER where source_table='160') m, 
+	cohort_cdm.NHID_60T n
 	where m.key_seq=n.key_seq
 	and m.seq_no=n.seq_no) b
 where b.master_seq=left(a.drug_exposure_id, 10)
@@ -226,7 +225,7 @@ and a.person_id=b.person_id;
 ---------------------------------------------------
 
 -- Sourec data: 30T
-INSERT INTO @NHISNSC_database.COST
+INSERT INTO cohort_cdm.COST
 	(cost_id, cost_event_id, cost_domain_id, cost_type_concept_id, currency_concept_id,
 	total_charge, total_cost, total_paid, paid_by_payer, paid_by_patient,
 	paid_patient_copay, paid_patient_coinsurance, paid_patient_deductible, paid_by_primary, paid_ingredient_cost,
