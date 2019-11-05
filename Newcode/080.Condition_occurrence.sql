458590e10c04fd2608d19c54d52883915747b83a
gustj258 <39437380+gustj258@users.noreply.github.com>
2019-11-05 10:20:36 +0900

    Update 080.Condition_occurrence.sql

--- Newcode/080.Condition_occurrence.sql
+++ Newcode/080.Condition_occurrence.sql
@@ -3,15 +3,15 @@
  --Author: SW Lee
  --Date: 2018.09.11
  
- @NHISNSC_rawdata: DB containing NHIS National Sample cohort DB
- @NHISNSC_database : DB for NHIS-NSC in CDM format
+ @NHIDNSC_rawdata: DB containing NHIS National Sample cohort DB
+ @NHIDNSC_database : DB for NHIS-NSC in CDM format
  @Mapping_database : DB for mapping table
- @NHIS_JK: JK table in NHIS NSC
- @NHIS_20T: 20 table in NHIS NSC
- @NHIS_30T: 30 table in NHIS NSC
- @NHIS_40T: 40 table in NHIS NSC
- @NHIS_60T: 60 table in NHIS NSC
- @NHIS_GJ: GJ table in NHIS NSC
+ @NHID_JK: JK table in NHIS NSC
+ @NHID_20T: 20 table in NHIS NSC
+ @NHID_30T: 30 table in NHIS NSC
+ @NHID_40T: 40 table in NHIS NSC
+ @NHID_60T: 60 table in NHIS NSC
+ @NHID_GJ: GJ table in NHIS NSC
  @CONDITION_MAPPINGTABLE : mapping table between KCD and SNOMED-CT
  --Description: Create Condition_occurrence table
  --Generating Table: CONDITION_OCCURRENCE
@@ -22,7 +22,7 @@
 ***************************************/ 
 /*
 CREATE TABLE cohort_cdm.CONDITION_OCCURRENCE ( 
-     condition_occurrence_id		BIGINT			PRIMARY KEY, 
+     condition_occurrence_id		NUMBER			PRIMARY KEY, 
      person_id						INTEGER			NOT NULL , 
      condition_concept_id			INTEGER			NOT NULL , 
      condition_start_date			DATE			NOT NULL , 
@@ -30,7 +30,7 @@ CREATE TABLE cohort_cdm.CONDITION_OCCURRENCE (
      condition_type_concept_id		INTEGER			NOT NULL , 
      stop_reason					VARCHAR(20), 
      provider_id					INTEGER, 
-     visit_occurrence_id			BIGINT, 
+     visit_occurrence_id			NUMBER, 
      condition_source_value			VARCHAR(50),
 	 condition_source_concept_id	VARCHAR(50)
 );
@@ -46,8 +46,8 @@ where a.invalid_reason is null and b.invalid_reason is null and a.domain_id='con
 ;
 
 select a.source_code, a.target_concept_id, a.domain_id, REPLACE(a.invalid_reason, '', NULL) as invalid_reason
-into #mapping_table2
-from @Mapping_database.source_to_concept_map a join @Mapping_database.CONCEPT b on a.target_concept_id=b.concept_id
+into mapping_table2
+from cohort_cdm.source_to_concept_map a join cohort_cdm.CONCEPT b on a.target_concept_id=b.concept_id
 where a.invalid_reason is null and b.invalid_reason is null
 ;
 
@@ -65,12 +65,12 @@ where a.invalid_reason is null and b.invalid_reason is null
 ***************************************/ 
 -- Using data only between observation_period & visiti_occurrence 
 --((299,311,028), 00:50:39)
-INSERT INTO @NHISNSC_database.CONDITION_OCCURRENCE
+INSERT INTO cohort_cdm.CONDITION_OCCURRENCE
 	(condition_occurrence_id, person_id, condition_concept_id, condition_start_date, condition_end_date,
 	condition_type_concept_id, stop_reason, provider_id, visit_occurrence_id, condition_source_value, 
 	condition_source_concept_id)
 select
-	convert(bigint, convert(bigint, m.master_seq) * 10 + convert(bigint, ROW_NUMBER() OVER(partition BY key_seq, seq_no order by target_concept_id desc))) as condition_occurrence_id,
+	to_number(m.master_seq) * 10 || ROW_NUMBER() OVER(partition BY key_seq, seq_no order by target_concept_id desc))) as condition_occurrence_id,
 	--ROW_NUMBER() OVER(partition BY key_seq, seq_no order by concept_id desc) AS rank, m.seq_no,
 	m.person_id as person_id,
 	n.target_concept_id as condition_concept_id,
@@ -85,9 +85,9 @@ select
 from (
 	select
 		a.master_seq, a.person_id, a.key_seq, a.seq_no, b.recu_fr_dt,
-		case when b.form_cd in ('02', '2', '04', '06', '07', '10', '12') and b.vscn > 0 then DATEADD(DAY, b.vscn-1, convert(date, b.recu_fr_dt , 112)) 
-			when b.form_cd in ('02', '2', '04', '06', '07', '10', '12') and b.vscn = 0 then DATEADD(DAY, cast(b.vscn as int), convert(date, b.recu_fr_dt , 112)) 
-			when b.form_cd in ('03', '3', '05', '08', '8', '09', '9', '11', '13', '20', '21', 'ZZ') and b.in_pat_cors_type in ('11', '21', '31') and vscn > 0 then DATEADD(DAY, b.vscn-1, convert(date, b.recu_fr_dt, 112)) 
+		case when b.form_cd in ('02', '2', '04', '06', '07', '10', '12') and to_date(b.recu_fr_dt , 'yyyymmdd') + b.vscn -1
+			when b.form_cd in ('02', '2', '04', '06', '07', '10', '12') and b.vscn = 0 then to_date(b.recu_fr_dt , 'yyyymmdd') + b.vscn -1 
+			when b.form_cd in ('03', '3', '05', '08', '8', '09', '9', '11', '13', '20', '21', 'ZZ') and b.in_pat_cors_type in ('11', '21', '31') and vscn > 0 then DATEADD(DAY, b.vscn-1, convert(b.recu_fr_dt, 'yyyymmdd') + b.vscn -1 
 			when b.form_cd in ('03', '3', '05', '08', '8', '09', '9', '11', '13', '20', '21', 'ZZ') and b.in_pat_cors_type in ('11', '21', '31') and vscn = 0 then DATEADD(DAY, cast(b.vscn as int), convert(date, b.recu_fr_dt, 112)) 
 			else convert(date, b.recu_fr_dt, 112)
 		end as visit_end_date,
